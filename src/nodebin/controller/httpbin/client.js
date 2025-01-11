cb2cb1e970f49af05fb72c7c4969c2545e3c445d
Arnab Sengupta <arnab10panda@gmail.com>
2025-01-11 15:06:37 +0530

    feat: application deployment file update

--- src/nodebin/controller/httpbin/client.js
+++ src/nodebin/controller/httpbin/client.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 
 const httpbinsvc = axios.create({
-    baseURL: 'https://httpbin.org/',
+    baseURL: `${process.env.HTTPBIN_URI}`,
 });
 
 module.exports = {httpbinsvc};
